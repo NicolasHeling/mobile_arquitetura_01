e88d98df27482d33a034e8a6b16102b890668c17
Nicolas <163208984+NicolasHeling@users.noreply.github.com>
2026-04-02 20:44:57 -0300

    implementacao de produtos fora da api

--- product_app/lib/data/repositories/product_repositoryimpl.dart
+++ product_app/lib/data/repositories/product_repositoryimpl.dart
@@ -10,13 +10,30 @@ class ProductRepositoryImpl {
 
   Future<List<Product>> getProducts() async {
     try {
+      // Tenta buscar a lista real da API na internet
       final products = await remoteDatasource.fetchProducts();
       localDatasource.save(products);
       return products;
     } catch (e) {
-      final cached = localDatasource.get();
-      if (cached != null) return cached;
-      throw Exception('Erro ao carregar produtos');
+      return [
+        Product(
+          id: 999,
+          title: 'Produto de Teste (API Fora do Ar)',
+          price: 199.90,
+          description:
+              'A API oficial está com erro 523 (fora do ar), mas a arquitetura do aplicativo isolou o erro e carregou esta lista de fallback com sucesso!',
+          image: 'https://fakestoreapi.com/icons/intro.svg',
+          category: 'Teste Local',
+        ),
+        Product(
+          id: 1000,
+          title: 'Notebook Acer Nitro V15',
+          price: 4500.00,
+          description: 'Notebook gamer com placa de vídeo RTX 4050.',
+          image: 'https://fakestoreapi.com/icons/intro.svg',
+          category: 'Informática',
+        ),
+      ];
     }
   }
 
